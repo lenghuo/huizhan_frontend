f2948e6d09e899520e93b7318f785b13daf9808c
ipenge <weixupeng999@163.com>
2023-12-15 15:03:43 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import './styles/index.css'
-import './styles/common.css'
+// import './styles/common.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
